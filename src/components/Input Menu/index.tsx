7022fcef8a7be54d9b03659d6ce336407ff8062a
adi wicaksono <muhammadiwicaksono@gmail.com>
2025-03-07 15:09:19 +0700

    fix : ui home and form

--- src/components/Input Menu/index.tsx
+++ src/components/Input Menu/index.tsx
@@ -9,9 +9,9 @@ interface InputProps {
 
 const InputMenu: FC<InputProps> = ({ title, placeholder, name }) => {
     return (
-        <label htmlFor={placeholder} className="flex flex-col gap-3 pb-3 border-b-2">
+        <label htmlFor={placeholder} className="flex flex-col gap-3">
             <p className="text-lg font-normal">{title}</p>
-            <Field type="text" placeholder={placeholder} name={name}/>
+            <Field type="text" placeholder={placeholder} name={name} className="text-[27px] focus:outline-none pb-3 border-b-2"/>
             <ErrorMessage name={name} component="div" className="text-red-500 text-xs"/>
         </label>
 
--- src/components/Sliding Content/index.tsx
+++ src/components/Sliding Content/index.tsx
@@ -5,7 +5,7 @@ const SlidingContent: FC = () => {
 
     return (
         <div className="flex justify-center items-center relative z-20 h-screen">
-            <p className="text-white text-[150px] font-medium whitespace-nowrap overflow-hidden absolute bottom-24">
+            <p className="text-white text-[150px] font-medium whitespace-nowrap overflow-hidden absolute bottom-24 running-text">
                 Webflow Developer - UI/UX Designer
             </p>
         </div>
